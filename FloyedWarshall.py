119e2d14bf18d6d2d223781fb302c8cda3c95fc5
Saud Hashmi <Saud.Hashmi@bentley.com>
2021-02-12 15:49:21 +0500

    improved weight assign

--- FloyedWarshall.py
+++ FloyedWarshall.py
@@ -1,6 +1,7 @@
 
 import sys
 import string
+import numpy as np
 
 #Driver Code 
 #sys.stdout.write("Maximal item set with total value " + str(totalvalue) + " and weight " + str(W)+":")
@@ -35,7 +36,7 @@ for i in range(2 ,len(score_ints),3):
 max_size = max(score_ints)
 #print(max_size)
 #print(weight)
-print(score_ints)
+#print(score_ints)
 
 # Using above second method to create a  
 # 2D array 
@@ -56,15 +57,45 @@ score_ints = remove_values_from_list(score_ints, 0)
 
 print(score_ints)
 
-for i in range(0,len(score_ints),2):
-    if(i < len(score_ints)):
-        #print(score_ints[i])
-        #print(score_ints[i+1])
-        print(i);
-        arr[score_ints[i]][score_ints[i+1]] = weight[i]
-        
+n = 2
+out = [score_ints[k:k+n] for k in range(0, len(score_ints), n)] 
+
+#print(out)
+
+j = len(out)
+
+
+for i in range (len(arr)):
+    for j in range(i):
+        print( "III"+str(i),"JJJ" + str(j))
+        #arr[out[rows]][out[cols]] = weight[rows]
 
 print(arr)
+# while j>0:
+#     arr[score_ints[i]][score_ints[i+1]] = weight[i]
+
+
+# for i in range(max_size):
+#     for k in range(j)
+#         arr[out[i][0]][out[i][1]] = weight[i]
+        
+
+# print(arr)
+
+
+
+# for i in range(0,(max_size),2):
+#     if(i < len(score_ints)):
+#         print(score_ints[i])
+#         print(score_ints[i+1])
+#         #print(i);
+#         arr[out[i][0]][out[i][1]] = weight[i]
+        
+
+# #print(arr)
+        
+       
+        
 
 for i in range(len(score_ints)):
     if (i%2)==0:
@@ -122,20 +153,20 @@ def floydWarshall(graph):
                 dist[i][j] = min(dist[i][j],
                                  dist[i][k] + dist[k][j]
                                  )
-        printSolution(dist,k)
+        #printSolution(dist,k)
  
  
 # A utility function to print the solution
 def printSolution(dist,i):
-    print "Iteration " + str(i)
+    print ("Iteration " + str(i))
     for i in range(V):
         for j in range(V):
             if(dist[i][j] == INF):
-                print "%7s" % ("-"),
+                print ("%7s" % ("-"),)
             else:
-                print "%7d\t" % (dist[i][j]),
+                print ("%7d\t" % (dist[i][j]),)
             if j == V-1:
-                print ""
+                print ("")
  
  
 # Driver program to test the above program
